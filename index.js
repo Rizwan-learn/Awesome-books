d97a0f9152370f8bbaa3efcdb20b88afbbc43cfe
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-03-15 07:37:56 +0000

    create Book list

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
-const stored = localStorage.getItem('arr');
-let arr = stored ? JSON.parse(stored) : [];
+// ARRAY OF OBJECTS TO STORE A LIST OF BOOKS
 
-document.getElementById('book').innerHTML = arr.map(e => `<li>${e.Title}</br> ${e.Author}</li><button id="btn">${e.Button}</button><hr>`).join(' ')
+const bookCollection = [];
 
-function toStore() {
-    let books = {
-        Title: '',
-        Author: '',
-        Button: 'remove'
+// Add book function
+const form = document.getElementById('book-form');
+const { title, author } = form.elements;
+
+const addBook = function () {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = {
+      title: title.value,
+      author: author.value,
     };
-    books.Title = document.getElementById('title').value;
-    books.Author = document.getElementById('author').value;
-    arr.push(books);
-    let node = document.createTextNode(books.Title);
-    document.getElementById('book').appendChild(node);
-    localStorage.setItem('arr', JSON.stringify(arr));
+
+    bookCollection.push(formData);
+    localStorage.setItem('bookData', JSON.stringify(bookCollection));
+    window.location.reload();
+  });
+};
+
+// PRESERVE DATA IN THE BROWSER (LOCAL STORAGE)
+
+const fillForm = localStorage.getItem('bookData');
+
+if (fillForm) {
+  const data = JSON.parse(localStorage.getItem('bookData'));
+
+  bookCollection.push(...data);
 }
-document.getElementById('button').addEventListener('click', toStore);
+
+// CREATING BOOKS FROM ARRAY DATA AND POPULATING DYNAMICALLY
+
+const container = document.querySelector('.container');
+
+bookCollection.forEach((book) => {
+  const content = `<div class="book">
+<p>${book.title}</p>
+<p>${book.author}</p>
+<button class="remove">Remove</button>
+<hr />
+</div>`;
+
+  container.innerHTML += content;
+});
+
+// Remove book function
+
+const removeBook = function () {
+  const remove = document.querySelectorAll('.remove');
+  const book = document.querySelectorAll('.book');
+
+  remove.forEach((button, i) => {
+    button.addEventListener('click', () => {
+      book[i].remove();
+      bookCollection.splice(i, 1);
+      localStorage.setItem('bookData', JSON.stringify(bookCollection));
+    });
+  });
+};
+
+addBook();
+removeBook();
