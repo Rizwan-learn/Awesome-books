452d4fd225a3e8ec03d6612f502ecdc418b028b2
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-03-15 14:51:39 +0000

    update js

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
 class Book {
-    constructor(title, author) {
-        this.title = title;
-        this.author = author;
-    }
-    getHtml() {
-        return `<div class="book">
-      <p>${this.title}</p>
-      <p>${this.author}</p>
-      <button class="remove">Remove</button>
-      <hr />
-      </div>`;
-    }
+  constructor(title, author) {
+      this.title = title;
+      this.author = author;
+  }
+  getHtml() {
+      return `<div class="book">
+    <p>${this.title}</p>
+    <p>${this.author}</p>
+    <button class="remove">Remove</button>
+    <hr />
+    </div>`;
+  }
 }
 
 class Books {
-    constructor() {
-        this.books = []
-    }
-    initialize() {
-        const dataString = localStorage.getItem('bookData');
-        if (dataString) {
-            this.books = JSON.parse(dataString).map(book => new Book(book.title, book.author));
-            this.setHtml()
-        }
-        this.setupRemove()
+  constructor() {
+      this.books = []
+  }
+  initialize() {
+      const dataString = localStorage.getItem('bookData');
+      if (dataString) {
+          this.books = JSON.parse(dataString).map(book => new Book(book.title, book.author));
+          this.setHtml()
+      }
+      this.setupRemove()
 
-    }
+  }
 
-    setupRemove() {
-        const remove = document.querySelectorAll('.remove');
-        const book = document.querySelectorAll('.book');
+  setupRemove() {
+      const remove = document.querySelectorAll('.remove');
+      const book = document.querySelectorAll('.book');
 
-        remove.forEach((button, i) => {
-            button.addEventListener('click', () => {
-                book[i].remove();
-                this.books.splice(i, 1);
-                localStorage.setItem('bookData', JSON.stringify(this.books));
-            });
-        });
-    }
+      remove.forEach((button, i) => {
+          button.addEventListener('click', () => {
+              book[i].remove();
+              this.books.splice(i, 1);
+              localStorage.setItem('bookData', JSON.stringify(this.books));
+          });
+      });
+  }
 
-    newBook(title, author) {
-        let book = new Book(title, author);
-        this.books.push(book)
-        localStorage.setItem('bookData', JSON.stringify(this.books));
-        this.setHtml();
-        return book
-    }
+  newBook(title, author) {
+      let book = new Book(title, author);
+      this.books.push(book)
+      localStorage.setItem('bookData', JSON.stringify(this.books));
+      this.setHtml();
+      return book
+  }
 
-    getBookList() {
-        let containerHtml = '';
-        this.books.forEach((book) => {
-            containerHtml += book.getHtml();
-        });
-        return containerHtml;
-    }
+  getBookList() {
+      let containerHtml = '';
+      this.books.forEach((book) => {
+          containerHtml += book.getHtml();
+      });
+      return containerHtml;
+  }
 
-    setHtml() {
-        const container = document.querySelector('.container');
-        container.innerHTML = this.getBookList();
-        this.setupRemove()
-    }
+  setHtml() {
+      const container = document.querySelector('.container');
+      container.innerHTML = this.getBookList();
+      this.setupRemove()
+  }
 
 }
 
@@ -69,15 +69,15 @@ const bookRepo = new Books();
 bookRepo.initialize();
 
 const addBook = function() {
-    let form = document.getElementById('book-form');
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const { title, author } = form.elements;
-        bookRepo.newBook(title.value, author.value);
+  let form = document.getElementById('book-form');
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const { title, author } = form.elements;
+      bookRepo.newBook(title.value, author.value);
 
-        title.value = ''
-        author.value = ''
-    });
+      title.value = ''
+      author.value = ''
+  });
 };
 
 addBook();
