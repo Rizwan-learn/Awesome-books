15b889c4131edd7ec9341522ea0155c355944ad8
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-03-22 10:54:57 +0000

    update refactor for index,js

--- index.js
+++ index.js
@@ -1,92 +1,16 @@
-import Book from './index2.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
+import Books from './modules/removeBooks.js';
+import spa from './modules/spa.js';
 
-const d = new Date();
-document.getElementById('time').innerHTML = d.toUTCString();
-document.getElementById('firstBlock').style.display = 'block';
-document.getElementById('secondBlock').style.display = 'none';
-document.getElementById('contact').style.display = 'none';
-
-document.getElementById('first').addEventListener('click', () => {
-  document.getElementById('firstBlock').style.display = 'block';
-  document.getElementById('secondBlock').style.display = 'none';
-  document.getElementById('contact').style.display = 'none';
-  document.getElementById('first').style.color = 'blue';
-  document.getElementById('third').style.color = 'black';
-  document.getElementById('second').style.color = 'black';
-}, false);
-
-document.getElementById('second').addEventListener('click', () => {
-  document.getElementById('firstBlock').style.display = 'none';
-  document.getElementById('secondBlock').style.display = 'block';
-  document.getElementById('contact').style.display = 'none';
-  document.getElementById('second').style.color = 'blue';
-  document.getElementById('third').style.color = 'black';
-  document.getElementById('first').style.color = 'black';
-}, false);
-document.getElementById('third').addEventListener('click', () => {
-  document.getElementById('firstBlock').style.display = 'none';
-  document.getElementById('secondBlock').style.display = 'none';
-  document.getElementById('contact').style.display = 'block';
-  document.getElementById('third').style.color = 'blue';
-  document.getElementById('second').style.color = 'black';
-  document.getElementById('first').style.color = 'black';
-}, false);
-
-class Books {
-  constructor() {
-    this.books = [];
-  }
-
-  initialize() {
-    const dataString = localStorage.getItem('bookData');
-    if (dataString) {
-      this.books = JSON.parse(dataString).map((book) => new Book(book.title, book.author));
-      this.setHtml();
-    }
-    this.setupRemove();
-  }
-
-  setupRemove() {
-    const remove = document.querySelectorAll('.remove');
-    const book = document.querySelectorAll('.book');
-
-    remove.forEach((button, i) => {
-      button.addEventListener('click', () => {
-        book[i].remove();
-        this.books.splice(i, 1);
-        localStorage.setItem('bookData', JSON.stringify(this.books));
-      });
-    });
-  }
-
-  newBook(title, author) {
-    const book = new Book(title, author);
-    this.books.push(book);
-    localStorage.setItem('bookData', JSON.stringify(this.books));
-    this.setHtml();
-    return book;
-  }
-
-  getBookList() {
-    let containerHtml = '';
-    this.books.forEach((book) => {
-      containerHtml += book.getHtml();
-    });
-    return containerHtml;
-  }
-
-  setHtml() {
-    const container = document.querySelector('.container');
-    container.innerHTML = this.getBookList();
-    this.setupRemove();
-  }
-}
-
+const dateBox = document.getElementById('time');
+setInterval(() => {
+  dateBox.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+}, 1000);
 const bookRepo = new Books();
 
 bookRepo.initialize();
 
-const addBook = function () {
+const addBook = () => {
   const form = document.getElementById('book-form');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -99,3 +23,4 @@ const addBook = function () {
 };
 
 addBook();
+spa();
